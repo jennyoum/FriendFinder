14e1b8f5793b208458ee876ab02dd9eb9d0f333f
Jenny Oum <jennyoum@hotmail.com>
2019-08-16 00:36:43 -0400

    added images for friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,23 +1,23 @@
 var friendsData = [
     {name:"Cat",
     values:[2, 3, 5, 8, 9, 8, 5, 4, 3, 1],
-    photo:""},
+    photo:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQS3TOLDVUlOQ9iiwa6qK7Le_c4RJ-hE1jZJ5zQKPo40cfP_Fg"},
 
     {name:"Dog",
     values:[4, 3, 8, 8, 9, 1, 4, 9, 1, 1],
-    photo:""},
+    photo:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQM3niYxqIczYuVqcNddAI17P38F5pwZUc4pyyaY6YT6FjkRG0C"},
 
     {name:"Rabbit",
     values:[1, 3, 5, 3, 2, 8, 6, 2, 4, 2],
-    photo:""},
+    photo:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0E5MGAi29Usff8znvne_fbdqORJ6ygEZaGS6l3DxXXqoXfm2d"},
 
     {name:"Hamster",
     values:[9, 1, 4, 6, 2, 5, 1, 9, 4, 9],
-    photo:""},
+    photo:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKuOb53qasao2aSh3CACp1Afph7ZR8xARkG4iWa6ORVq2H1ome"},
 
-    {name:"Lizard",
+    {name:"Goldfish",
     values:[9, 1, 4, 6, 2, 5, 1, 9, 4, 9],
-    photo:""},
+    photo:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTk2jwY59I9fm5naUn6J8pr5gUbLwdUj-VQndS0xf0hxICwXePIyg"},
 ]
   
 module.exports = friendsData;
